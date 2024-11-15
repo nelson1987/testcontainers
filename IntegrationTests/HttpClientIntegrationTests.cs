68468e36938982dd8c321b5409e17121d599943d
Nelson Neto <nelson.ash@outlook.com>
2024-11-15 14:40:46 -0300

    Funcional

--- IntegrationTests/HttpClientIntegrationTests.cs
+++ IntegrationTests/HttpClientIntegrationTests.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Presentation;
 using Presentation.Commons;
 using RabbitMQ.Client;
@@ -11,28 +12,30 @@ using Testcontainers.MsSql;
 using Testcontainers.RabbitMq;
 
 namespace IntegrationTests;
+
 public class IntegrationTestWebAppFactory
-    : WebApplicationFactory<Program>,
-        IAsyncLifetime
+    : WebApplicationFactory<Program>
 {
-    private readonly MsSqlContainer _msSqlContainer = new MsSqlBuilder()
-        .WithImage("mcr.microsoft.com/mssql/server:2022-latest")
-        .WithPassword("Strong_password_123!")
+    private readonly RabbitMqContainer _rabbitMqContainer = new RabbitMqBuilder()
+        .WithImage("rabbitmq:3-alpine")
         .Build();
 
-    private readonly RabbitMqContainer _rabbitMqContainer = new RabbitMqBuilder()
-        .WithImage("rabbitmq:3.11")
-        .Build();
-    
     public IntegrationTestWebAppFactory()
     {
         _rabbitMqContainer.StartAsync().Wait();
     }
+    
+    // protected override void ConfigureClient(HttpClient client)
+    // {
+    //     base.ConfigureClient(client);       
+    // }
+
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
+        //_rabbitMqContainer.StartAsync().GetAwaiter();
         builder.ConfigureTestServices(services =>
         {
-            
+            services.RemoveAll<ConnectionFactory>();
             services.AddSingleton(sp =>
             {
                 var uri = new Uri(_rabbitMqContainer.GetConnectionString());
@@ -41,31 +44,10 @@ public class IntegrationTestWebAppFactory
                     Uri = uri,
                 };
             });
-            //     var descriptorType =
-            //         typeof(DbContextOptions<ApplicationDbContext>);
-            //
-            //     var descriptor = Enumerable
-            //         .SingleOrDefault<ServiceDescriptor>(services, s => s.ServiceType == descriptorType);
-            //
-            //     if (descriptor is not null) services.Remove(descriptor);
-            //
-            //     EntityFrameworkServiceCollectionExtensions.AddDbContext<ApplicationDbContext>(services, options =>
-            //         SqlServerDbContextOptionsExtensions.UseSqlServer(options, _dbContainer.GetConnectionString()));
         });
     }
-
-    public async Task InitializeAsync()
-    {
-        await _msSqlContainer.StartAsync();
-        await _rabbitMqContainer.StartAsync();
-    }
-
-    public async Task DisposeAsync()
-    {
-        await _msSqlContainer.StopAsync();
-        await _rabbitMqContainer.StopAsync();
-    }
 }
+
 public class HttpClientIntegrationTests
 {
     [Fact]
