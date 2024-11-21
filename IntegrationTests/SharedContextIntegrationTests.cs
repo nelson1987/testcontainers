0ee0d6b560a79e2176dacdff73eae4e6fe8bc797
Nelson Neto <nelson.neto@db1.com.br>
2024-11-21 15:39:19 -0300

    Criacao de Domain

--- IntegrationTests/SharedContextIntegrationTests.cs
+++ IntegrationTests/SharedContextIntegrationTests.cs
@@ -1,6 +1,7 @@
 using System.Reflection;
 using System.Text;
 using System.Text.Json;
+using Domain;
 using FluentAssertions;
 using FluentAssertions.Extensions;
 using Microsoft.AspNetCore.Hosting;
@@ -57,7 +58,6 @@ public class IntegrationTestWebAppFactory
     }
 }
 
-// Classe de contexto do EF Core
 public class TestDbContext : DbContext
 {
     public TestDbContext(DbContextOptions<TestDbContext> options) : base(options)
@@ -102,32 +102,6 @@ public class CustomerMapConfiguration : IEntityTypeConfiguration<Customer>
         builder.Property(e => e.Age)
             .IsRequired();
     }
-}
-
-// Modelos
-public class Customer
-{
-    protected Customer()
-    {
-    }
-
-    public Customer(int id, string name, string email, int age)
-    {
-        ArgumentOutOfRangeException.ThrowIfLessThan(id, 0);
-        ArgumentException.ThrowIfNullOrWhiteSpace(name);
-        ArgumentException.ThrowIfNullOrWhiteSpace(email);
-        ArgumentOutOfRangeException.ThrowIfLessThan(age, 18);
-        Id = id;
-        Name = name;
-        Email = email;
-        Age = age;
-    }
-
-    public int Id { get; private set; }
-    public string Name { get; private set; }
-    public string Email { get; private set; }
-    public int Age { get; private set; }
-    public List<Order> Orders { get; set; } = new();
 }
 
 public class CustomerUnitTests
@@ -226,33 +200,6 @@ public class OrderUnitTests
     }
 }
 
-public class Order
-{
-    protected Order()
-    {
-    }
-
-    public Order(int id, DateTime orderDate, decimal total, Customer customer)
-    {
-        ArgumentOutOfRangeException.ThrowIfLessThan(id, 0);
-        ArgumentOutOfRangeException.ThrowIfLessThan(orderDate, DateTime.UtcNow.AddSeconds(-5));
-        ArgumentOutOfRangeException.ThrowIfLessThan(total, 0.01M);
-        ArgumentNullException.ThrowIfNull(customer);
-        Id = id;
-        OrderDate = orderDate;
-        Total = total;
-        //CustomerId = customerId;
-        Customer = customer;
-    }
-
-    public int Id { get; private set; }
-    public DateTime OrderDate { get; private set; }
-    public decimal Total { get; private set; }
-    public int CustomerId { get; private set; }
-    public Customer Customer { get; private set; }
-}
-
-// Classe que encapsula toda a infraestrutura compartilhada
 public class SharedTestInfrastructure : IAsyncLifetime
 {
     private readonly MsSqlContainer _sqlContainer;
@@ -360,13 +307,11 @@ public class SharedTestInfrastructure : IAsyncLifetime
     }
 }
 
-// Coleção compartilhada para os testes
 [CollectionDefinition("Shared Infrastructure")]
 public class SharedInfrastructureCollection : ICollectionFixture<SharedTestInfrastructure>
 {
 }
 
-// Classe base para testes que compartilham a infraestrutura
 public abstract class IntegrationTestBase
 {
     protected readonly TestDbContext DbContext;
@@ -410,15 +355,6 @@ public class SharedInfrastructure : IntegrationTestBase, IAsyncDisposable
     }
 }
 
-public interface IUnitOfWork
-{
-    Task BeginTransactionAsync();
-    Task CommitAsync();
-    Task RollbackAsync();
-    ICustomerRepository Customers { get; }
-    IOrderRepository Orders { get; }
-}
-
 public class UnitOfWork : IUnitOfWork
 {
     private readonly TestDbContext _context;
@@ -451,12 +387,6 @@ public class UnitOfWork : IUnitOfWork
         => new OrderRepository(_context);
 }
 
-public interface ICustomerRepository
-{
-    Task AddCustomerAsync(Customer customer);
-    Task<Customer?> GetCustomerAsync(Customer customer);
-}
-
 public class CustomerRepository : ICustomerRepository
 {
     private readonly TestDbContext _context;
@@ -475,12 +405,6 @@ public class CustomerRepository : ICustomerRepository
     public async Task<Customer?> GetCustomerAsync(Customer customer) => await _context
         .Set<Customer>()
         .FirstOrDefaultAsync(c => c.Email == customer.Email);
-}
-
-public interface IOrderRepository
-{
-    Task AddOrderAsync(Order order);
-    Task<Order?> GetOrderAsync(Order order);
 }
 
 public class OrderRepository : IOrderRepository
@@ -504,26 +428,6 @@ public class OrderRepository : IOrderRepository
         .FirstOrDefaultAsync(o => o.Id == order.Id);
 }
 
-public class DomainEvent<T>
-    where T : class
-{
-    public string EventType = typeof(T).FullName;
-    public string EventId = Guid.NewGuid().ToString("D");
-    public DateTime EventDate = DateTime.UtcNow;
-
-    public DomainEvent(T message)
-    {
-        Message = message;
-    }
-
-    public T Message { get; private set; }
-}
-
-public record CreatedCustomerEvent(int CustomerId);
-
-public record CreatedOrderEvent(int OrderId);
-
-// Primeira classe de testes
 public class CustomerIntegrationTests : SharedInfrastructure
 {
     private const string QueueName = "customer_events";
@@ -581,49 +485,6 @@ public class CustomerIntegrationTests : SharedInfrastructure
     }
 }
 
-public interface IOrderDomainService
-{
-    
-}
-
-public class OrderDomainService : IOrderDomainService
-{
-    private readonly IUnitOfWork _unitOfWork;
-    private readonly IProducer<CreatedOrderEvent> _createdOrderproducer;
-
-    public OrderDomainService(IUnitOfWork unitOfWork, IProducer<CreatedOrderEvent> createdOrderproducer)
-    {
-        _unitOfWork = unitOfWork;
-        _createdOrderproducer = createdOrderproducer;
-    }
-
-    public async Task AddOrderAsync(Order order)
-    {
-        try
-        {
-            await _unitOfWork.BeginTransactionAsync();
-            await _unitOfWork.Customers.AddCustomerAsync(order.Customer);
-            await _unitOfWork.Orders.AddOrderAsync(order);
-            await _unitOfWork.CommitAsync();
-            await _createdOrderproducer.Send(order.ToCreatedEvent());
-        }
-        catch (Exception ex)
-        {
-            await _unitOfWork.RollbackAsync();
-        }
-    }
-
-    public async Task<Order?> FindOrderAsync(Order order)
-        => await _unitOfWork.Orders.GetOrderAsync(order);
-}
-
-public static class CreatedOrderEventExtensions
-{
-    public static DomainEvent<CreatedOrderEvent> ToCreatedEvent(this Order order)
-        => new DomainEvent<CreatedOrderEvent>(new CreatedOrderEvent(order.Id));
-}
-
-// Segunda classe de testes
 public class OrderIntegrationTests : SharedInfrastructure
 {
     private readonly OrderDomainService _orderDomainService;
@@ -710,13 +571,6 @@ public class BrokerIntegrationTests : SharedInfrastructure
     }
 }
 
-public interface IConsumer<T> where T : class
-{
-    TaskCompletionSource<bool> messageReceived { get; }
-    TaskCompletionSource<string> messageEventReceived { get; }
-    Task Consume(string queueName);
-}
-
 public class Consumer<T> : IConsumer<T> where T : class
 {
     public TaskCompletionSource<bool> messageReceived { get; }
@@ -750,11 +604,6 @@ public class Consumer<T> : IConsumer<T> where T : class
             autoAck: false,
             consumer: consumerEvent);
     }
-}
-
-public interface IProducer<T> where T : class
-{
-    Task Send(DomainEvent<T> message);
 }
 
 public class Producer<T> : IProducer<T> where T : class
