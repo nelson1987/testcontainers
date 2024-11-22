4f564d0fd059c0cd5e9e4fcafb6e5d3fc41f35c3
Nelson Neto <nelson.neto@db1.com.br>
2024-11-22 14:27:40 -0300

    Teste de Integracao

--- IntegrationTests/Commons/SharedInfrastructure.cs
+++ IntegrationTests/Commons/SharedInfrastructure.cs
@@ -1,3 +1,5 @@
+using Domain;
+using Infrastructure;
 using RabbitMQ.Client;
 
 namespace IntegrationTests;
@@ -6,15 +8,24 @@ namespace IntegrationTests;
 public class SharedInfrastructure : IntegrationTestBase, IAsyncDisposable
 {
     protected readonly IChannel Channel;
+    protected readonly IConsumer<CreatedOrderEvent> CreatedOrderConsumer;
 
     public SharedInfrastructure(SharedTestInfrastructure infrastructure)
         : base(infrastructure)
     {
-        Channel = RabbitConnection.CreateChannelAsync().GetAwaiter().GetResult();
+        Channel = RabbitConnection.CreateChannelAsync()
+            .GetAwaiter()
+            .GetResult();
+        Channel.QueueDeclareAsync(typeof(CreatedOrderEvent).FullName!, durable: true, exclusive: false,
+                autoDelete: false)
+            .GetAwaiter()
+            .GetResult();
+        CreatedOrderConsumer = new Consumer<CreatedOrderEvent>(Channel);
     }
 
     public async ValueTask DisposeAsync()
     {
         await Channel.DisposeAsync();
+        await Channel.QueueDeleteAsync(typeof(CreatedOrderEvent).FullName!);
     }
 }
---
+++ IntegrationTests/OrderControllerIntegrationTests.cs
@@ -0,0 +1,36 @@
+using System.Text;
+using System.Text.Json;
+using Application;
+using Domain;
+using FluentAssertions;
+using Presentation.Commons;
+
+namespace IntegrationTests;
+
+public class OrderControllerIntegrationTests : SharedInfrastructure
+{
+    public OrderControllerIntegrationTests(SharedTestInfrastructure infrastructure) 
+        : base(infrastructure)
+    {
+    }
+
+    [Fact]
+    public async Task GetCustomerDetails_ShouldCallInternalApi()
+    {
+        // Assert
+        var command = new CreateOrderCommand("John Doe", "johndoe@email.com",18,100.00M);
+        var serializeCommand = JsonSerializer.Serialize(command);
+        var content = new StringContent(serializeCommand, Encoding.UTF8, "application/json");
+        // Act
+        var response = await Client.PostAsync(Constantes.GET_URI_PATH, content);
+        // Assert
+        response.Should().BeSuccessful();
+        
+        await CreatedOrderConsumer.Consume().WaitAsync(TimeSpan.FromSeconds(5));
+        //var messageReceived = await CreatedOrderConsumer.MessageReceived.Task.WaitAsync(TimeSpan.FromSeconds(5));
+        var messageEventReceived = await CreatedOrderConsumer.MessageEventReceived.Task.WaitAsync(TimeSpan.FromSeconds(5));
+        var @event = JsonSerializer.Deserialize<DomainEvent<CreatedOrderEvent>>(messageEventReceived);
+        //messageReceived.Should().BeTrue();
+        @event!.Message.OrderId.Should().Be(1);
+    }
+}
